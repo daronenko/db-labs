8bfcd510dfac103633f1f133e2ee560ff8db7b12
Daniil Mironenko <dan.mironenko@gmail.com>
2023-11-01 21:32:30 +0300

    solve some rk2 tasks

---
+++ rk2-solutions.sql
@@ -0,0 +1,73 @@
+-- Вариант 1
+
+-- 1. Начальники отделов, являющиеся главными по работе
+SELECT ФИО FROM СОТРУДНИКИ
+    WHERE НОМЕР_СТ IN (SELECT ГЛАВНЫЙ FROM РАБОТЫ)
+    AND НОМЕР_СТ IN (SELECT НАЧАЛЬНИК FROM ОТДЕЛЫ);
+
+SELECT ФИО FROM СОТРУДНИКИ
+    WHERE НОМЕР_СТ IN (SELECT ГЛАВНЫЙ FROM РАБОТЫ
+        INNER JOIN ОТДЕЛЫ
+        ON РАБОТЫ.ГЛАВНЫЙ = ОТДЕЛЫ.НАЧАЛЬНИК);
+
+-- 2. Работы, у которых нет сотрудников
+SELECT НАЗВАНИЕ FROM РАБОТЫ
+    WHERE НОМЕР_РБ NOT IN (SELECT РАБОТА FROM СПИСКИ_СТ);
+
+-- 3. Сотрудники, получающие заработную плату максимальную в отделе
+SELECT ФИО FROM СОТРУДНИКИ employees_1
+    WHERE ЗАРПЛАТА = (SELECT MAX(ЗАРПЛАТА) FROM СОТРУДНИКИ employees_2
+        WHERE employees_1.ОТДЕЛ = employees_2.ОТДЕЛ);
+
+SELECT ФИО FROM СОТРУДНИКИ employees
+    INNER JOIN
+        (SELECT MAX(ЗАРПЛАТА) AS max_salary, ОТДЕЛ FROM СОТРУДНИКИ
+            GROUP BY ОТДЕЛ) max_salaries
+    ON max_salaries.max_salary = employees.ЗАРПЛАТА
+    AND max_salaries.ОТДЕЛ = employees.ОТДЕЛ;
+
+
+-- Вариант 2
+
+-- 1. Сотрудники, у которых руководители являются начальниками отделов
+SELECT ФИО FROM СОТРУДНИКИ
+    WHERE РУКОВОД IN (SELECT НАЧАЛЬНИК FROM ОТДЕЛЫ);
+
+-- 2. Сколько работ выполняет каждый отдел
+SELECT COUNT(НОМЕР_СТ) AS jobs_count, ОТДЕЛ FROM СОТРУДНИКИ
+    WHERE НОМЕР_СТ IN (SELECT ГЛАВНЫЙ FROM РАБОТЫ)
+GROUP BY ОТДЕЛ;
+
+-- 3. Главные по работам, получающие заработную плату большую, чем их начальник отдела
+
+SELECT employees_1.ФИО FROM СОТРУДНИКИ employees_1
+    WHERE employees_1.ЗАРПЛАТА > (SELECT employees_2.ЗАРПЛАТА FROM СОТРУДНИКИ AS employees_2
+        WHERE employees_2.НОМЕР_СТ = (SELECT department.НАЧАЛЬНИК FROM ОТДЕЛЫ AS department
+            WHERE department.НОМЕР_ОТ = employees_1.ОТДЕЛ)
+    AND employees_2.НОМЕР_СТ IS NOT NULL);
+
+
+-- Вариант 3
+
+-- 1. Сотрудники, у которых руководитель является главным по работе
+SELECT ФИО FROM СОТРУДНИКИ
+    WHERE РУКОВОД IN (SELECT ГЛАВНЫЙ FROM РАБОТЫ);
+
+-- 2. Работы, у которых количество задействованных сотрудников меньше 3
+SELECT jobs.НАЗВАНИЕ AS РАБОТА FROM РАБОТЫ AS jobs
+    WHERE (SELECT COUNT(*) FROM СПИСКИ_СТ AS employees_list
+        WHERE employees_list.РАБОТА = jobs.НОМЕР_РБ) < 3;
+
+-- 3. Максимальная зп сотрудника по каждой из работ
+SELECT РАБОТЫ.НАЗВАНИЕ AS РАБОТА, MAX(СОТРУДНИКИ.ЗАРПЛАТА) AS "МАКСИМАЛЬНАЯ ЗП" FROM РАБОТЫ, СПИСКИ_СТ, СОТРУДНИКИ
+    WHERE РАБОТЫ.НОМЕР_РБ = СПИСКИ_СТ.РАБОТА
+    AND СПИСКИ_СТ.СОТР = СОТРУДНИКИ.НОМЕР_СТ
+GROUP BY РАБОТЫ.НОМЕР_РБ, РАБОТЫ.НАЗВАНИЕ ORDER BY РАБОТЫ.НОМЕР_РБ;
+
+
+-- Дополнительные задания
+
+-- 1. Количество сотруднков, принимающих участие в каждой из работ
+SELECT НАЗВАНИЕ, COUNT(*) AS employees FROM РАБОТЫ, СОТРУДНИКИ, СПИСКИ_СТ
+    WHERE НОМЕР_СТ = СОТР
+    AND РАБОТА = НОМЕР_РБ GROUP BY 1;
