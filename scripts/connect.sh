cf2485dddab7e5b25380209e0d9b950233e27f37
Daniil Mironenko <dan.mironenko@gmail.com>
2023-11-22 11:38:32 +0300

    update scripts

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 USER=root
-PASSWORD=1234
 DB_NAME=$1
 
-psql -h localhost -d "$DB_NAME" -U $USER -a
+psql -h localhost -d $DB_NAME -U $USER -a
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 USER=root
-PASSWORD=1234
+PASSWORD=123
 DB_NAME=$1
 
 docker run \
-    --name postgres-stand -p 5432:5432 \
+    --name $DB_NAME -p 5432:5432 \
     -e POSTGRES_USER=$USER \
     -e POSTGRES_PASSWORD=$PASSWORD \
-    -e POSTGRES_DB="$DB_NAME" \
-    -d postgres:16-alpine \
-&& sleep 3 \
-&& psql -h localhost -d "$DB_NAME" -U $USER -a
+    -e POSTGRES_DB=$DB_NAME \
+    -d postgres:16-alpine
