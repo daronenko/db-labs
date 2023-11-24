53d53f6f0fdba5b9f4bc7eb48d79d11c40d58f7b
Daniil Mironenko <dan.mironenko@gmail.com>
2023-11-24 23:20:19 +0300

    comment creating indexes in 'text-search.sql'

--- lab-3/queries/text-search.sql
+++ lab-3/queries/text-search.sql
@@ -5,9 +5,9 @@ ADD COLUMN _name_tsvector tsvector;
 UPDATE store.softwares
 SET _name_tsvector = to_tsvector('english', name);
 
-CREATE INDEX idx_name_search
-ON store.softwares
-USING gin(_name_tsvector);
+-- CREATE INDEX idx_name_search
+-- ON store.softwares
+-- USING gin(_name_tsvector);
 
 SELECT * FROM store.softwares
 WHERE _name_tsvector @@ to_tsquery('english', 'car');
@@ -19,9 +19,9 @@ ADD COLUMN _description_tsvector tsvector;
 UPDATE store.softwares
 SET _description_tsvector = to_tsvector('english', description);
 
-CREATE INDEX idx_description_search
-ON store.softwares
-USING gin(_description_tsvector);
+-- CREATE INDEX idx_description_search
+-- ON store.softwares
+-- USING gin(_description_tsvector);
 
 SELECT * FROM store.softwares
 WHERE _description_tsvector @@ to_tsquery('english', 'pats');
