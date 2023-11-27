38d28f5e41994aaa82c5bb87780ad2e5d6379d1a
Daniil Mironenko <dan.mironenko@gmail.com>
2023-11-27 18:56:34 +0300

    update lab-4

--- lab-4/queries/index-analysis.sql
+++ lab-4/queries/index-analysis.sql
@@ -13,6 +13,7 @@ WHERE
 -- CREATE INDEX idx_tags ON "store"."softwares" USING GIN("tags");
 
 
+-- EXPLAIN ANALYZE
 SELECT
     softwares.name,
     reviews.rating,
@@ -21,7 +22,7 @@ FROM store.reviews
 JOIN store.softwares ON reviews.software_id = softwares.id
 WHERE
     softwares.category = 'education'
-    AND reviews.rating <= 3;
+    AND reviews.rating >= 4;
 
 -- CREATE INDEX idx_category ON "store"."softwares" ("category");
 -- CREATE INDEX idx_rating ON "store"."reviews" ("rating");
