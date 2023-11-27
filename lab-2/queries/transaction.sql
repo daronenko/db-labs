660c10d42464ea75a8aba058b4bea1937ddd1925
Daniil Mironenko <dan.mironenko@gmail.com>
2023-11-27 18:56:11 +0300

    update lab-2

--- lab-2/queries/transaction.sql
+++ lab-2/queries/transaction.sql
@@ -2,7 +2,19 @@
 BEGIN TRANSACTION ISOLATION LEVEL SERIALIZABLE;
 
 UPDATE store.softwares
-SET license_price = ROUND(license_price * 1.1)
+SET license_price = ROUND(license_price * 0.9)
 WHERE id = '5a50ed1a-9f98-4a69-a93d-dd196af1fcb2';
 
 COMMIT;
+
+INSERT INTO store.softwares VALUES (
+    'e914d404-a938-4108-a517-b3c953bbb565',
+    'Elegant 2',
+    'This is an awesome software!',
+    'education',
+    '{}'::varchar[],
+    '1.0.0',
+    '2023-11-26',
+    '10',
+    '10'
+);
